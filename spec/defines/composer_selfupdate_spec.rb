a3989ba764ed5b6b0af5e33c0565c863fe5b4621
Thomas Ploch <thomas.ploch@meinfernbus.de>
2014-02-21 18:42:21 +0100

    Updated test suite

---
+++ spec/defines/composer_selfupdate_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe 'composer::selfupdate' do
+  ['RedHat', 'Debian'].each do |osfamily|
+    context "on #{osfamily} operating system family" do
+      let(:facts) { {
+          :osfamily => osfamily,
+      } }
+
+      context 'with default params' do
+        it { should contain_class('composer') }
+        let(:title) { 'auto_update' }
+        
+        it {
+          should contain_exec('composer_selfupdate_auto_update').with({
+            :command => %r{php /usr/local/bin/composer selfupdate},
+            :tries   => 3,
+            :timeout => 300,
+          })
+        }
+      end
+      context 'with clean backup params' do
+        it { should contain_class('composer') }
+        let(:title) { 'another_update' }
+        let(:params) { {
+          :clean_backups => true,
+          :version       => '12354eaf43',
+        } }
+        it {
+          should contain_exec('composer_selfupdate_another_update').with({
+            :command => %r{php /usr/local/bin/composer selfupdate --clean-backups 12354eaf43},
+            :tries   => 3,
+            :timeout => 300,
+          })
+        }
+      end
+      context 'with rollback params' do
+        it { should contain_class('composer') }
+        let(:title) { 'rollback_update' }
+        let(:params) { {
+          :rollback => true,
+          :version  => '12354eaf43',
+        } }
+        it {
+          should contain_exec('composer_selfupdate_rollback_update').with({
+            :command => %r{php /usr/local/bin/composer selfupdate --rollback 12354eaf43},
+            :tries   => 3,
+            :timeout => 300,
+          })
+        }
+      end
+      context 'with rollback missing verion' do
+        let(:title) { 'rollback_without_version' }
+        let(:params) { {
+          :rollback => true,
+        } }
+        it 'should not compile' do
+          expect { should compile }.to raise_error(/You cannot use rollback without specifying a version/)
+        end
+      end
+    end
+  end
+end
+
